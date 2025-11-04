19724ebd30240b5b986a3596bd47d6e39497b941
Abhijit <kumarabhijeetpanda@rediffmail.com>
2025-11-04 11:07:47 +0530

    Refactored and created the basic user Service Microservice

--- src/main/java/com/abhijit/fitnessmicroservice/model/UserRole.java
+++
@@ -1,5 +0,0 @@
-package com.abhijit.fitnessmicroservice.model;
-
-public enum UserRole {
-    USER, ADMIN
-}
--- src/main/java/com/abhijit/fitnessmicroservice/repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.abhijit.fitnessmicroservice.repository;
-
-import com.abhijit.fitnessmicroservice.model.User;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface UserRepository extends JpaRepository<User, String> {
-    boolean existsByEmail(String email);
-}
