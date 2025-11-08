849ec59a2746a0fdd50c1eaaa60942ecaf008fdf
Abhijit <kumarabhijeetpanda@rediffmail.com>
2025-11-08 17:59:59 +0530

    Added activity-service as normal folder (removed submodule)

---
+++ activity-service/src/main/java/com/abhijit/activityservice/repository/ActivityRepository.java
@@ -0,0 +1,12 @@
+package com.abhijit.activityservice.repository;
+
+import com.abhijit.activityservice.model.Activity;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface ActivityRepository extends MongoRepository<Activity, String> {
+    List<Activity> findByUserId(String userId);
+}
